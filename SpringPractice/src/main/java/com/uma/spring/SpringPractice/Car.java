c9d7514c8d407e8e76da33cbb9f55df989128fd0
UMAMAHESWARI <UMAMAHESWARI@UMAMAHESWARI>
2021-04-02 22:12:55 +0530

    simple car example created. Car.java,TestCar.java,resources->applicationContext.xml

---
+++ SpringPractice/src/main/java/com/uma/spring/SpringPractice/Car.java
@@ -0,0 +1,9 @@
+package com.uma.spring.SpringPractice;
+
+public class Car {
+
+	public void drive()
+	{
+		System.out.println("driving car");
+	}
+}
--- SpringPractice/src/test/java/com/uma/spring/SpringPractice/TestCar.java
+++ SpringPractice/src/test/java/com/uma/spring/SpringPractice/TestCar.java
@@ -3,19 +3,16 @@ package com.uma.spring.SpringPractice;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class TestCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println( "Hello World!" );
         ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        /*Student student=(Student)context.getBean("studentBean");
-        student.displayInfo();*/
+       
         Car c=(Car)context.getBean("car");
         c.drive();
-    }
+
+	}
+
 }
